3e9e99815f242a6b7fd06246dd4fd0d38940a909
StMafiaJacky <lvtianxiang_dlut@163.com>
2022-09-29 11:23:54 +0900

    first commit

---
+++ src/test/java/com/brightstar/PandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
